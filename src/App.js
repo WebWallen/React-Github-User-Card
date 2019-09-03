d19228d624ed917793df461537eee4dbbcff28ae
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-09-03 16:25:05 -0400

    Redid the project, added errors to API data requests since it is a best practice, and explained what the code is doing with comments.

--- src/App.js
+++ src/App.js
@@ -7,51 +7,53 @@ import Search from "./components/Search";
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {
+    this.state = { // sets the initial state that should render when the app loads -- values can change later with setState
       userName: 'WebWallen',
       user: {},
-      followers: []
+      followers: [] // user and followers are set to match the data structure returned from the Github API
     };
   }
 
   changeUserName = (userName) => {
-    this.setState({ userName: userName })
+    this.setState({ userName }) // sets state to new username based on user's search criteria
   }
 
   fetchUser = () => {
-    fetch(`https://api.github.com/users/${this.state.userName}`)
-      .then(res => res.json()) // fetch only grabs the URL's general info, must explicitly convert to JSON and then request data
-      .then(data => this.setState({ user: data}))
+    fetch(`https://api.github.com/users/${this.state.userName}`) // template literal necessary when URL is based on the user's input
+      .then(res => res.json()) // fetch only grabs the URL's general info, must explicitly convert to JSON to get the specifics
+      .then(data => this.setState({ user: data})) // passes the user's info into the empty object declared in this.state
+      .catch(err => console.log(err));
   }
 
   fetchFollowers = () => {
     fetch(`https://api.github.com/users/${this.state.userName}/followers`)
       .then(res => res.json())
-      .then(data => this.setState({ followers: data}))
+      .then(data => this.setState ({ followers: data})) // passes the user's followers into the empty array in this.state
+      .catch(err => console.log(err));
   }
 
-  componentDidMount() {
+  componentDidMount() { // when the component mounts, fetch the user's profile information and followers
     this.fetchUser();
     this.fetchFollowers();
   }
 
   componentDidUpdate (prevProps, prevState) {
-    console.log(this.state); // sneaky way of accessing data set to state, which isn't available in the above method
-    if (prevState.userName !== this.state.userName) {
-      this.fetchUser();
-      this.fetchFollowers();
+    console.log(this.state); // If you want to confirm you're getting the right info, gotta console it here versus in ComponentDidMount
+    if (prevState.userName !== this.state.userName) { // if the user requests a profile other than the one currently in screen view...
+      this.fetchUser(); // Grab that profile's information and render it instead
+      this.fetchFollowers(); // ^ ditto with their followers
     }
   }
 
   render() {
     return (
       <div className="App">
-        <img src={logo} className="logo" alt="Github logo" />
-        <Search changeUserName={this.changeUserName} />
-        <UserCard user={this.state.user} followers={this.state.followers}/>
+        <img src={logo} className="logo" alt="Big, black and white Github logo" />
+        <Search changeUserName={this.changeUserName} />  {/* Remember: don't have to call state on functions or methods */}
+        <UserCard user={this.state.user} followers={this.state.followers} />
       </div>
     )
   }
 }
-
+  
 export default App;
--- src/components/Search.js
+++ src/components/Search.js
@@ -10,30 +10,30 @@ class Search extends React.Component {
     }
 
     handleChange = event => {
-        this.setState({ [event.target.name]: event.target.value })
+        this.setState ({ [event.target.name]: event.target.value })
     }
 
     handleSubmit = event => {
         event.preventDefault();
-        this.props.changeUserName(this.state.search);
-        this.setState({ search: "" })
+        this.props.changeUserName(this.state.search); // Attached to props because it's pulling username from the parent component App
+        this.setState({ search: "" }) // After submitting the form, clear the search bar for the user's convenience
     }
 
     render() {
         return (
             <form className="search-form" onSubmit={this.handleSubmit}>
                 <p>Search for Any User Below!</p>
-                <input type="text" 
-                       name="search" 
+                <input type="text"
+                       name="search"
                        className="search-input"
                        value={this.state.search}
                        onChange={this.handleChange}
-                       placeholder="Search criteria go here..." 
+                       placeholder="Search criteria go here..."
                 />
                 <button className="search-button" type="submit">Search for a User</button>
             </form>
         )
     }
 }
-
+   
 export default Search;
